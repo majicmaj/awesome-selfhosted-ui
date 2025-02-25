9fe89c82a5ffc8bbb1628862cd36eefb9cfb6a9a
majicmaj <majd.tarbin@outlook.com>
2025-02-25 17:55:00 -0500

    Remove unused imports from DiscoverPage component

--- src/pages/DiscoverPage.tsx
+++ src/pages/DiscoverPage.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ArrowRight, Dices } from "lucide-react";
 import { LoadingScreen } from "../components/LoadingScreen";
